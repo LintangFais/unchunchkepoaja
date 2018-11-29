1d20e9a4ba08f825205b80cf0a9dd6ec1926a99d
BeLikeDoge <36959327+TheCreeperIndo@users.noreply.github.com>
2018-11-29 20:23:59 +0700

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -7,12 +7,12 @@ exports.run = async(client, msg, args) => {
 		if(serverQueue.voiceChannel.id !== msg.member.voice.channelID) return msg.channel.send({ embed: { color: 0xf91d1d, description: `You must be in **${serverQueue.voiceChannel.name}** to change the current volume`}});	
 		if (!args[1]) return msg.channel.send({embed: { color: 0x32ffe7,  description: `The current volume is: **${serverQueue.volume}**%`}});
 		serverQueue.volume = args[1];
-    if (args[1] > 100) return msg.channel.send({ embed: { color: 0xFF0000, description: `${msg.author} Volume limit is 100%, please do not hurt yourself!`}});
+    if (args[1] > 500) return msg.channel.send({ embed: { color: 0xFF0000, description: `${msg.author} Volume limit is 500%, please do not hurt yourself!`}});
     serverQueue.volume = args[1];
     if (args[1] > 500) return !serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 500) +
     msg.channel.send({ embed: { color: 0xFF0000, description: `${msg.author} Volume limit is 500%, please do not hurt yourself!`}});
  
-    if (args[1] < 101) return serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 100) + msg.channel.send({ embed: { color: 0x008000, description: `I set the volume to: __**${args[1]}**%__`}});
+    if (args[1] < 500) return serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 500) + msg.channel.send({ embed: { color: 0x008000, description: `I set the volume to: __**${args[1]}**%__`}});
     
   }
 

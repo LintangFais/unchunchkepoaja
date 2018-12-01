06fa63a3ecebf236ce8e80edf043789c23a454f7
fatir1242 <39852500+fatir1242@users.noreply.github.com>
2018-12-01 14:59:40 +0700

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+
+exports.run = async (bot, msg, args) => {
+     let embed = new Discord.RichRmbed()
+     .setColor("RANDOM")
+     .setAuthor("Meliodas [BETA] Help !", bot.user.displayAvatarURL)
+     .addField("🎀 Core", "`shardinfo`, `stats`, `ev`, `ex`")
+     .addField("<a:MusicNote:513518117247254566> Music", "`play`, `stop`, `search`, `lyrics`, `loop`, `setbitrate`, `queue`, `pause`, `resume`, `skip`, `shuffle`, `nowplay`, `volume`")
+     .setFooter(`• Message For ${msg.author.tag}`, msg.author.displayAvatarURL)
+}
+
+exports.conf = {
+   aliases: ['h']
+}
+
+exports.help = {
+   name: 'help"
+}
